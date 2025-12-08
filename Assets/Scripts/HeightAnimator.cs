efc6c73cc59c2a17aacbc075bef66802745a97aa
RomanSharipov <roman.sharipov.1990@gmail.com>
2025-12-08 12:37:58 +0500

    remove needless  ForceRebuildLayoutImmediate

--- Assets/Scripts/HeightAnimator.cs
+++ Assets/Scripts/HeightAnimator.cs
@@ -6,17 +6,17 @@ using UnityEngine.UI;
 public class HeightAnimator : MonoBehaviour
 {
     [SerializeField] private RectTransform _targetRect;
+    [SerializeField] private LayoutElement _layoutElement;
     [SerializeField] private float _expandedHeight = 150f;
     [SerializeField] private float _animationDuration = 0.3f;
 
     private float _collapsedHeight;
     private Coroutine _currentAnimation;
-    private RectTransform _parentRect;
+
     
     private void Awake()
     {
         _collapsedHeight = _targetRect.sizeDelta.y;
-        _parentRect = _targetRect.parent as RectTransform;
     }
 
     public void Expand()
@@ -49,24 +49,17 @@ public class HeightAnimator : MonoBehaviour
             float t = elapsed / _animationDuration;
 
             float newHeight = Mathf.Lerp(startHeight, targetHeight, t);
-            _targetRect.sizeDelta = new Vector2(_targetRect.sizeDelta.x, newHeight);
 
-            RebuildLayout();
-
+            _layoutElement.preferredHeight = newHeight;
+            
             yield return null;
         }
 
         _targetRect.sizeDelta = new Vector2(_targetRect.sizeDelta.x, targetHeight);
-        RebuildLayout();
+        
 
         _currentAnimation = null;
     }
 
-    private void RebuildLayout()
-    {
-        if (_parentRect != null)
-        {
-            LayoutRebuilder.ForceRebuildLayoutImmediate(_parentRect);
-        }
-    }
+
 }
